96ab9745d5d7bb54c4e3200548721b4151561ce6
Kirk Kornegay <kkornegay1@gmail.com>
2020-11-15 20:04:52 -0500

    working to jsonify

---
+++ app.py
@@ -0,0 +1,86 @@
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+
+#################################################
+# Database Setup
+#################################################
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save reference to the table
+measurement = Base.classes.measurement
+station = Base.classes.station
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+
+#################################################
+# Flask Routes
+#################################################
+
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+    )
+
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of all passenger names"""
+    # Query all dates and precipitation
+    results = session.query(measurement.date, measurement.prcp).all()
+
+    session.close()
+
+    # Convert to Dictionary
+     precip = []
+    for date, prcp in results:
+        prcp_dict = {}
+        prcp_dict["date"] = date
+        prcp_dict["prcp"] = prcp
+        precip.append(prcp_dict)
+
+    return jsonify(precip)
+
+
+@app.route("/api/v1.0/stations")
+def stations():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of passenger data including the name, age, and sex of each passenger"""
+    # Query all stations
+    results = session.query(station.station).unique()
+
+    session.close()
+
+    # Create a list of stations
+    all_stations = list(np.ravel(results))
+    
+    return jsonify(all_stations)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
